b52f879e31781ef046288eac55fbb63ec908b8db
Mitja Gustin <mitja@popcom.si>
2013-08-14 15:16:04 +0200

    added htaccess file for easy deployment

--- config/db_create.sql
+++ config/db_create.sql
@@ -5,9 +5,9 @@ SET time_zone = "+00:00";
 -- Database for project
 --
 
-CREATE DATABASE celtra DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_bin;
+--CREATE DATABASE celtra DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_bin;
 
-USE celtra;
+--USE celtra;
 
 --
 -- Table structure for table `folders`
@@ -55,3 +55,93 @@ insert into folders values (null , 'folder 6');
 insert into folders values (null , 'folder 7');
 insert into folders values (null , 'folder 8');
 insert into folders values (null , 'folder 9');
+
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '1',  'file 1');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '1',  'file 2');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '1',  'file 3');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '1',  'file 4');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '1',  'file 5');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '1',  'file 6');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '1',  'file 7');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '1',  'file 8');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '1',  'file 9');
+
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '2',  'file 1');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '2',  'file 2');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '2',  'file 3');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '2',  'file 4');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '2',  'file 5');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '2',  'file 6');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '2',  'file 7');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '2',  'file 8');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '2',  'file 9');
+
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '3',  'file 1');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '3',  'file 2');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '3',  'file 3');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '3',  'file 4');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '3',  'file 5');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '3',  'file 6');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '3',  'file 7');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '3',  'file 8');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '3',  'file 9');
+
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '4',  'file 1');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '4',  'file 2');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '4',  'file 3');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '4',  'file 4');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '4',  'file 5');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '4',  'file 6');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '4',  'file 7');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '4',  'file 8');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '4',  'file 9');
+
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '5',  'file 1');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '5',  'file 2');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '5',  'file 3');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '5',  'file 4');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '5',  'file 5');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '5',  'file 6');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '5',  'file 7');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '5',  'file 8');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '5',  'file 9');
+
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '6',  'file 1');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '6',  'file 2');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '6',  'file 3');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '6',  'file 4');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '6',  'file 5');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '6',  'file 6');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '6',  'file 7');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '6',  'file 8');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '6',  'file 9');
+
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '7',  'file 1');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '7',  'file 2');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '7',  'file 3');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '7',  'file 4');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '7',  'file 5');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '7',  'file 6');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '7',  'file 7');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '7',  'file 8');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '7',  'file 9');
+
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '8',  'file 1');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '8',  'file 2');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '8',  'file 3');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '8',  'file 4');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '8',  'file 5');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '8',  'file 6');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '8',  'file 7');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '8',  'file 8');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '8',  'file 9');
+
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '9',  'file 1');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '9',  'file 2');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '9',  'file 3');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '9',  'file 4');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '9',  'file 5');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '9',  'file 6');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '9',  'file 7');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '9',  'file 8');
+INSERT INTO  `ads` (`id` , `folder_id` , `name` ) VALUES (NULL ,  '9',  'file 9');
