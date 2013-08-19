840327ad9504e033386264384307c415a9bfd5ef
Mitja Guštin <gustinmi@gmail.com>
2013-08-19 12:57:37 +0200

    Update constants.dev.php

--- config/constants.dev.php
+++ config/constants.dev.php
@@ -1,5 +1,5 @@
 <?php
     define("DB_SERVER", "127.0.0.1:3306");
-    define("DB_USER", "root");
-    define("DB_PASS", "jboss");
-    define("DB_NAME", "celtra");
+    define("DB_USER", "***");
+    define("DB_PASS", "***");
+    define("DB_NAME", "***");
