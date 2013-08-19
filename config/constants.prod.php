7aa80747b0574a87e2eefc4325a0f1f1d2620791
Mitja Guštin <gustinmi@gmail.com>
2013-08-19 12:57:19 +0200

    Update constants.prod.php

--- config/constants.prod.php
+++ config/constants.prod.php
@@ -2,6 +2,6 @@
     define ('phpee\env', 'production' );   #environment [prod | dev | test]
 
     define("DB_SERVER", "127.0.0.1:3306");
-    define("DB_USER", "root");
-    define("DB_PASS", "sitart5");
-    define("DB_NAME", "celtra");
+    define("DB_USER", "***");
+    define("DB_PASS", "***");
+    define("DB_NAME", "***");
