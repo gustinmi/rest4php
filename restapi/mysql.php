e38d62d805557705f5fea547cacd1322adda0d85
Mitja Gustin <gustinmi@gmail.com>
2015-11-05 15:10:37 -0500

    updated test mysql file

--- restapi/mysql.php
+++ restapi/mysql.php
@@ -1,7 +1,8 @@
 <?php
 
-// ONLY FOR TESTING
+require 'constants.php';
 
+// ONLY FOR TESTING
 $link = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 if (!$link) {
     die('Connect Error: ' . mysqli_connect_error());
